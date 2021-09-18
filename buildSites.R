4cc5d362b89d9f692e5e608c03d89378b2932f6a
John Everett <everett.jk@gmail.com>
2021-09-17 23:35:54 -0400

    buildSites

--- buildSites.R
+++ buildSites.R
@@ -1,17 +1,54 @@
 library(dplyr)
-library(yaml)
-library(parallel)
 
-sites <- readRDS('/home/everett/projects/AAVengeR/data/testData/sites.rds')
-
-opt <- read_yaml('config.yml')
+opt <- yaml::read_yaml('config.yml')
 source(file.path(opt$softwareDir, 'lib.R'))
 
-frags <- readRDS(file.path(opt$outputDir, 'mapLeaderSequences', 'frags.rds'))
+dir.create(file.path(opt$outputDir, 'buildSites'))
+
+frags <- readRDS(file.path(opt$outputDir, 'standardizeFragments', 'frags.rds'))
 
 frags$posid <- paste0(frags$seqnames, frags$strand, ifelse(frags$strand == '+', frags$start, frags$end))
 
-# # Collapse replicate replicate fragments to sample replicates.
-# o <- lapply(split(frags, paste(frags$subject, frags$sample, frags$seqnames, frags$start, frags$end)), function(x){
-#    if(nrow(x) > 1)browser()
-# })
+if('buildSites_excludeSites' %in% names(opt)){
+  p <- unlist(lapply(strsplit(unlist(strsplit(opt$buildSites_excludeSites, '\\|')), ','), function(x){
+    if(length(x) != 3) return()
+    subset(frags, seqnames == x[1] & start >= x[2] & start <= x[3])$posid
+  }))
+  
+  frags <- subset(frags, ! posid %in% p)
+}
+
+
+sites <- bind_rows(lapply(split(frags, paste(frags$subject, frags$sample, frags$posid)), function(x){
+  if(nrow(x) > 1){
+    i <- rep(TRUE, nrow(x))
+    
+    # Check that leader sequences are all similar now that we are combining fragments from different replicates.
+    r <- representativeSeq(x$repLeaderSeq)
+    
+    if(r[[1]] > opt$buildFragments_maxLeaderSeqDiffScore){
+      # There is a conflict, one or more fragments have a markedly different leader sequences then the other fragments.
+      # Attempt to salvage this site by retaining the majority of fragments with similiar leader sequences.
+      
+      i <- as.vector(stringdist::stringdistmatrix(x$repLeaderSeq, r[[2]]) / nchar(x$repLeaderSeq) < opt$buildFragments_maxLeaderSeqDiffScore)
+      if(sum(i)/nrow(x) >= opt$buildFragments_assemblyConflictResolution){
+        x <- x[i,]
+      } else {
+        return(tibble())
+      }
+    }
+    
+    r <- representativeSeq(x$repLeaderSeq)
+
+    return(dplyr::mutate(x, estAbund = n_distinct(width), position = ifelse(strand[1] == '+', start[1], end[1]), 
+                  reads = sum(reads), repLeaderSeq = r[[2]], fragmentsRemoved = sum(!i)) %>%
+    dplyr::select(subject, sample, seqnames, strand, position, posid, estAbund, reads, fragmentsRemoved, repLeaderSeq) %>%
+    dplyr::slice(1))
+    }else{
+    
+    return(dplyr::mutate(x, estAbund = n_distinct(width), position = ifelse(strand[1] == '+', start[1], end[1]), fragmentsRemoved = 0) %>%
+             dplyr::select(subject, sample, seqnames, strand, position, posid, estAbund, reads, fragmentsRemoved, repLeaderSeq))
+  }
+}))
+
+saveRDS(sites, file.path(opt$outputDir, 'buildSites', 'sites.rds'))
---
+++ mapSiteLeaderSequences.R
@@ -0,0 +1,79 @@
+library(ShortRead)
+library(dplyr)
+library(parallel)
+options(stringsAsFactors = FALSE)
+
+opt <- yaml::read_yaml('config.yml')
+
+source(file.path(opt$softwareDir, 'lib.R'))
+
+sites <- readRDS(file.path(opt$outputDir, 'buildSites', 'sites.rds'))
+
+dir.create(file.path(opt$outputDir, 'mapSiteLeaderSequences'))
+
+dir.create(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp'))
+dir.create(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'dbs'))
+
+samples <- loadSamples()
+
+sites$s <- paste(sites$subject, sites$sample)
+
+m <- bind_rows(lapply(split(samples, samples$anchorRead.identification), function(x){
+  invisible(file.remove(list.files(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'dbs'), full.names = TRUE)))
+  
+  o <- strsplit(unlist(strsplit(x$anchorRead.identification[1], ';')), ',')
+  d <- DNAStringSet(unlist(lapply(o, '[', 2)))
+  names(d) <- unlist(lapply(o, '[', 1))
+  writeXStringSet(d, file.path(opt$outputDir, 'mapSiteLeaderSequences', 'dbs', paste0('d.fasta')))
+  
+  system(paste0(opt$command_makeblastdb, ' -in ', file.path(opt$outputDir, 'mapSiteLeaderSequences', 'dbs', 'd.fasta'), 
+                ' -dbtype nucl -out ', file.path(opt$outputDir, 'mapSiteLeaderSequences', 'dbs', 'd')), ignore.stderr = TRUE)
+  
+  waitForFile(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'dbs', 'd.nin'))
+  
+  s <- subset(sites, s %in% unique(paste(x$subject, x$sample)))
+  
+  reads <- DNAStringSet(unique(s$repLeaderSeq))
+  names(reads) <- paste0('s', 1:length(reads))
+  
+  b <- bind_rows(lapply(mixAndChunkSeqs(reads, opt$mapLeaderSequences_alignmentChunkSize), function(a){
+    f <- tmpFile()
+ 
+    writeXStringSet(a,  file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp', paste0(f, '.fasta')))
+    
+    system(paste0(opt$command_blastn, ' -word_size 4 -evalue 50 -outfmt 6 -query ',
+                  file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp', paste0(f, '.fasta')), ' -db ',
+                  file.path(opt$outputDir, 'mapSiteLeaderSequences', 'dbs', 'd'),
+                  ' -num_threads 4 -out ', file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp', paste0(f, '.blast'))),
+           ignore.stdout = TRUE, ignore.stderr = TRUE)
+    
+    waitForFile(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp', paste0(f, '.blast')))
+    
+    if(file.info(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp', paste0(f, '.blast')))$size == 0) return(tibble())
+    
+    b <- read.table(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp', paste0(f, '.blast')), sep = '\t', header = FALSE)
+    names(b) <- c('qname', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore')
+    b$alignmentLength <- b$qend - b$qstart + 1
+    
+    b <- dplyr::filter(b, pident >= opt$mapLeaderSequences_minAlignmentPercentID, alignmentLength >= opt$mapLeaderSequences_minAlignmentLength)
+
+    bind_rows(lapply(split(b, b$qname), function(x){
+      x <- subset(x, evalue == min(x$evalue)) 
+      x <- subset(x, alignmentLength == max(x$alignmentLength))
+      
+      tibble(id = x$qname[1], leaderMaping.id = paste(unique(x$sseqid), collapse = '|'), 
+             leaderMapping.qStart = min(x$qstart), leaderMapping.qEnd = max(x$qend),
+             leaderMapping.sStart = min(x$sstart), leaderMapping.sEnd = max(x$send))
+    }))
+  }))
+  
+  r <- tibble(id = names(reads), leaderSeq = as.character(reads))
+  left_join(r, b, by = 'id')
+})) %>% tidyr::drop_na()
+
+invisible(file.remove(list.files(file.path(opt$outputDir, 'mapSiteLeaderSequences', 'tmp'), full.names = TRUE)))
+
+sites <- select(sites, -s) %>% left_join(select(m, -id), by = c('repLeaderSeq' = 'leaderSeq'))
+
+saveRDS(sites, file.path(opt$outputDir, 'mapSiteLeaderSequences', 'sites.rds'))
+
