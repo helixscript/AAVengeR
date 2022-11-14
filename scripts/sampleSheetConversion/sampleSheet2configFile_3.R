2f6d2649ba653a77a2499b10dc1b0b84438a7230
Ubuntu <ubuntu@ip-10-128-254-29.ec2.internal>
2022-11-14 20:02:52 +0000

    Example update

--- scripts/sampleSheetConversion/sampleSheet2configFile_3.R
+++ scripts/sampleSheetConversion/sampleSheet2configFile_3.R
@@ -2,10 +2,10 @@ library(dplyr)
 library(stringr)
 library(RMySQL)
 
-sampleSheet <- '/home/ubuntu/projects/CD4zeta/data/SampleSheet.csv'
-outputFile  <- '/home/ubuntu/projects/CD4zeta/samplData.tsv'
+sampleSheet <- '/home/ubuntu/projects/Persaud/201123_M03249_0121_000000000-D6KV5/data/SampleSheet.csv'
+outputFile  <- '/home/ubuntu/projects/Persaud/201123_M03249_0121_000000000-D6KV5/data/sampleData.tsv'
 refGenome.id <- 'hg38'
-trialID <- 'CD4zeta'
+trialID <- 'Persaud'
 
 f <- readLines(sampleSheet)
 s <- read.csv(textConnection(f[(which(grepl('\\[metaData\\]', f))+1):length(f)]), header = TRUE)
@@ -16,21 +16,22 @@ dbConn  <- dbConnect(MySQL(), group='specimen_management')
 gtsp <- dbGetQuery(dbConn, 'select * from gtsp')
 dbDisconnect(dbConn)
 
-samples <- sub('\\-\\d+$', '', s$alias)
+samples <- sub('\\-\\d+$', '', s$SampleName)
+reps <- as.integer(sub('\\-', '', stringr::str_extract(samples, '\\-\\d+')))
+flags <- ifelse(grepl('u5', samples, ignore.case = TRUE), 'IN_u5', 'IN_u3')
+samples <- stringr::str_extract(samples, 'GTSP\\d+')
 subjects <- gtsp[match(samples, gtsp$SpecimenAccNum),]$Patient
-i <- which(is.na(subjects))
-subjects[i] <- sub('\\-\\d+$', '', samples[i])
 
 r <- tibble(trial = trialID,
             subject = subjects,
             sample = samples,
-            replicate = str_extract(s$alias, '(\\d+)$'),
-            index1.seq = s$bcSeq,
+            replicate = reps,
+            index1.seq = s$barcode,
             refGenome.id = refGenome.id,
-            adriftRead.linker.seq = s$linkerSequence,
-            leaderSeqHMM = 'cd4Zeta.hmm', 
-            vectorFastaFile = 'vector_ZETA.fa',
-            flags = 'IN_u5')
+            adriftRead.linker.seq = s$linker,
+            leaderSeqHMM = ifelse(flags == 'IN_u5', 'u5_100.hmm', 'u3_100_rc.hmm'), 
+            vectorFastaFile = 'HIV-1_reference.fasta',
+            flags = flags)
             
 write.table(r, file = outputFile, sep='\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
 
